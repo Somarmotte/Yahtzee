dc30c59071cfff9f8a7cac7441a6321c657e09b6
Somarmotte <121799919+Somarmotte@users.noreply.github.com>
2023-02-28 15:29:09 +0100

    Add files via upload

---
+++ Yahtzee/src/test/java/LanceDesTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+
+public class LanceDesTest {
+
+    @Test
+    public void lanceDes(){
+        fail("not good");
+
+    }
+}
